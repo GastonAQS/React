001ade025b093ebefea77b9f67bd298178821355
root <root@localhost.localdomain>
2019-01-22 20:28:37 -0300

    changes

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,5 +1,9 @@
+import jsonplaceholder from "../apis/jsonplaceholder";
+
 export const fetchPosts = () => {
-  return {
-    type: "FETCH_POSTS"
+  return async dispatch => {
+    const response = await jsonplaceholder.get("/posts");
+
+    dispatch({ type: "FETCH_POSTS", payload: response });
   };
 };
