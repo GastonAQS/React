b8902cb646962d96d3a19dd244c58985e45a4cb8
root <root@localhost.localdomain>
2019-01-23 20:18:36 -0300

    file changes

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,9 +1,22 @@
 import jsonplaceholder from "../apis/jsonplaceholder";
+import _ from "lodash";
 
 export const fetchPosts = () => {
   return async dispatch => {
     const response = await jsonplaceholder.get("/posts");
 
-    dispatch({ type: "FETCH_POSTS", payload: response });
+    dispatch({ type: "FETCH_POSTS", payload: response.data });
   };
 };
+
+export const fetchUser = id => async dispatch => {
+  const response = await jsonplaceholder.get(`/users/${id}`);
+
+  dispatch({ type: "FETCH_USER", payload: response.data });
+};
+// eslint-disable-next-line
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonplaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// });
