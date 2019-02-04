2dd2b261f62d71f257dd449e2a323a3f5a51d3c4
root <root@localhost.localdomain>
2019-02-04 10:19:42 -0300

    changes

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from "../apis/streams";
+import history from "../history";
 
 export const signIn = userId => {
   return {
@@ -13,9 +14,11 @@ export const signOut = () => {
   };
 };
 
-export const createStream = formValues => async dispatch => {
-  const response = await streams.post("/streams", formValues);
+export const createStream = formValues => async (dispatch, getState) => {
+  const { userId } = getState().auth;
+  const response = await streams.post("/streams", { ...formValues, userId });
   dispatch({ type: "CREATE_STREAM", payload: response.data });
+  history.push("/");
 };
 
 export const fetchStreams = () => async dispatch => {
@@ -31,6 +34,7 @@ export const fetchStream = id => async dispatch => {
 export const editStream = (id, formValues) => async dispatch => {
   const response = await streams.put(`/streams/${id}`, formValues);
   dispatch({ type: "EDIT_STREAM", payload: response.data });
+  history.push("/");
 };
 
 export const deleteStream = id => async dispatch => {
